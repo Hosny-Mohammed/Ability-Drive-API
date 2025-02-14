bce790a5f49694cdc32256bc845df51fef92773c
Hosny-Mohammed <hosnymohammed2007@gmail.com>
2025-02-14 16:42:35 +0200

    add lastKnownLocation in seeding Func and add second migration

---
+++ Ability Drive API/Migrations/20250214144125_second.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ability_Drive_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Drivers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LastKnownLocation",
+                value: "Cairo");
+
+            migrationBuilder.UpdateData(
+                table: "Drivers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "LastKnownLocation",
+                value: "Giza");
+
+            migrationBuilder.UpdateData(
+                table: "Drivers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "LastKnownLocation",
+                value: "Alexandria");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Drivers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LastKnownLocation",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Drivers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "LastKnownLocation",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Drivers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "LastKnownLocation",
+                value: null);
+        }
+    }
+}
