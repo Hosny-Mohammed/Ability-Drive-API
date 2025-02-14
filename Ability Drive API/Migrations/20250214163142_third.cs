88e7e2de398b8a87a3e7992052086c97667f1942
Hosny-Mohammed <hosnymohammed2007@gmail.com>
2025-02-14 18:32:29 +0200

    add third migration

---
+++ Ability Drive API/Migrations/20250214163142_third.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ability_Drive_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "SeatBookings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "SeatBookings",
+                type: "int",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
